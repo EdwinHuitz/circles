e24d969f41369730b2349aece882c7f5277c61ef
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-09-22 15:21:22 -0400

    Updates

---
+++ precache-manifest.68456f473b4f1ac6f0303d3dfed26605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf833f19c841292df0efbb729a3ce5",
+    "url": "/circles/index.html"
+  },
+  {
+    "revision": "7525232a5099fa9d4f75",
+    "url": "/circles/static/css/main.dc6919c0.chunk.css"
+  },
+  {
+    "revision": "0a111f232ec354c6f98d",
+    "url": "/circles/static/js/2.655bf65e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/circles/static/js/2.655bf65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7525232a5099fa9d4f75",
+    "url": "/circles/static/js/main.991fc835.chunk.js"
+  },
+  {
+    "revision": "9cf4b856db87aa01eb61",
+    "url": "/circles/static/js/runtime-main.439f5b84.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d31b740685e3bc976df4cc23b6eab86.js"
+  "/circles/precache-manifest.68456f473b4f1ac6f0303d3dfed26605.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/circles/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
