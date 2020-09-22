ac27b00bcd3f7eafa6f18d476b1b51531cd6ab8e
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-09-22 15:14:41 -0400

    Updates

---
+++ precache-manifest.0d31b740685e3bc976df4cc23b6eab86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db8ec68c7db569a31d7f98b8ad744f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc98d90bb0f11770e610",
+    "url": "/static/css/main.dc6919c0.chunk.css"
+  },
+  {
+    "revision": "0a111f232ec354c6f98d",
+    "url": "/static/js/2.655bf65e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.655bf65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc98d90bb0f11770e610",
+    "url": "/static/js/main.ce27b16b.chunk.js"
+  },
+  {
+    "revision": "4c89efdfdb16efe18cfc",
+    "url": "/static/js/runtime-main.fa1441d3.js"
+  }
+]);
